e043b8e0869d5c0d084dbcc4d23c531f9014e114
Non39564 <sarawut.on.62@ubu.ac.th>
2020-12-17 23:08:10 +0700

    add node01

--- nodejs/Node01/nodeserver.js
+++ nodejs/Node01/nodeserver.js
@@ -1,7 +1,15 @@
 const http = require('http');
 http.createServer((req, res) => {
-    console.log('request agin!!')
-    res.write('sawaddeee kub');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(`<html>
+        <head>
+        </head>
+        <body>
+            <h1>Sawaddee kub tai sa ma chik</h1>
+            <span> testing </span>
+        </body>
+        </html>
+        `);
     res.end();
 }).listen(1111);
 console.log('one');
